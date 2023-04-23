b7aadd1fcb58c714e9fa9e49650e471b935d5d11
Kirill Novikov <92146975+NeSmotriPojaluista@users.noreply.github.com>
2023-04-23 20:58:45 +0300

    1st commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+        
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="js/jquery.cookies.js"></script>
+        <script><?php include "js/scripts.js";?></script>
+
+        <style><?php include "css/styles.css";?></style>
+    </head>
+    <body>
+        <div class="header">Авторизация</div>
+        
+        <div class="auth-content">
+            <div class="auth-form">
+                <input type="text" class="auth-input" placeholder="Логин"/>
+                <input type="text" class="auth-input" placeholder="Пароль"/>
+                <input type="button" class="auth-submit" value="Войти"/>
+            </div>
+            
+        </div>
+        
+    </body>
+</html>
