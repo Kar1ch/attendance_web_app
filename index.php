76d13811601db9a937dbfad2846820175b64cbf2
Kar1ch <90093350+Kar1ch@users.noreply.github.com>
2023-05-01 23:50:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
 
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js%22%3E</script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js</script>
         <script src="js/jquery.cookies.js"></script>
         <script><?php include "js/scripts.js";?></script>
 
