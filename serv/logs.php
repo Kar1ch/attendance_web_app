4002917f68f8183368f244e238e5479db09f8b8c
hitfot <90093220+hitfot@users.noreply.github.com>
2023-05-27 07:59:21 +0300

    1
    Логи final version

--- serv/logs.php
+++ serv/logs.php
@@ -3,17 +3,19 @@
     function AddLog($type, $user, $func, $description){
         try{
             $fname = 'log.txt';
-            $file = fopen($fname, 'w');
+            $file = fopen($fname, 'c');
+            fseek($file, 0, SEEK_END);
             $line = '';
             $time = date("Y-m-d H:i:s");
             $type = $type == 0 ? 'ERROR' : 'OK';
-            $line =  $time." | [".$type."] | ".$user." | ".$func." | ".$description."\n";
-            fwrite($file, $line);
+            $line =  $time." | [".$type."] | ".$user." | ".$func." | ".$description;
+            fwrite($file, PHP_EOL.$line);   
+            fclose($file);
             return 1;
         }
         catch(Exception $e){
             return 0;
         }
     }
-    
+
 ?>
